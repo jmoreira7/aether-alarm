143cb53523d848ab73fa79cc3709076e9305cd2d
jmoreira7 <jonas.andrade7@gmail.com>
2025-05-11 10:35:59 +0200

    Supress unchecked cast lint warning

--- app/src/main/java/com/example/snoozeloo/ui/MainViewModelFactory.kt
+++ app/src/main/java/com/example/snoozeloo/ui/MainViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.snoozeloo.SnoozelooApp
 
 class MainViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MainViewModel(SnoozelooApp.appModule.alarmRepository) as T
     }

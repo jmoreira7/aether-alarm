81448a7b391239ab7063ca7fd56a7cc370fe7d6e
jmoreira7 <jonas.andrade7@gmail.com>
2025-05-11 22:52:24 +0200

    Improve text input fields

--- app/src/main/java/com/example/snoozeloo/ui/createalarm/CreateAlarmActivity.kt
+++ app/src/main/java/com/example/snoozeloo/ui/createalarm/CreateAlarmActivity.kt
@@ -1,8 +1,10 @@
 package com.example.snoozeloo.ui.createalarm
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.snoozeloo.R
 import com.example.snoozeloo.databinding.ActivityCreateAlarmBinding
 
 class CreateAlarmActivity : AppCompatActivity() {
@@ -13,5 +15,38 @@ class CreateAlarmActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityCreateAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        setupHourTextInputView()
+        setupMinuteTextInputView()
+    }
+
+    private fun setupHourTextInputView() {
+        binding.activityCreateAlarmHourTextInput.run {
+            setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    setTextColor(resources.getColor(R.color.dodger_blue, null))
+                    setText(EMPTY_STRING)
+                }
+            }
+        }
+    }
+
+    private fun setupMinuteTextInputView() {
+        binding.activityCreateAlarmMinuteTextInput.run {
+            setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    setTextColor(resources.getColor(R.color.dodger_blue, null))
+                    setText(EMPTY_STRING)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val EMPTY_STRING = ""
     }
 }
